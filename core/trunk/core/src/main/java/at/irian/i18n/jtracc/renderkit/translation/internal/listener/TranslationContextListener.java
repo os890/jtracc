499a52e49a017391452c6ec61d72ca27bf8ad26e
gerhard.petracek <gerhard.petracek@2f68b751-142f-0410-b4aa-05b97da1c307>
2007-06-28 15:57:32 +0000

    JSF RI support

--- core/trunk/core/src/main/java/at/irian/i18n/jtracc/renderkit/translation/internal/listener/TranslationContextListener.java
+++ core/trunk/core/src/main/java/at/irian/i18n/jtracc/renderkit/translation/internal/listener/TranslationContextListener.java
@@ -18,29 +18,23 @@
  */
 package at.irian.i18n.jtracc.renderkit.translation.internal.listener;
 
-import org.apache.myfaces.context.servlet.ServletExternalContextImpl;
 import at.irian.i18n.jtracc.custom.translationpane.HtmlTranslationPaneRenderer;
 import at.irian.i18n.jtracc.Settings;
 import at.irian.i18n.jtracc.renderkit.translation.HtmlTranslationCommandLink;
 import at.irian.i18n.jtracc.renderkit.translation.model.HtmlTargetLocaleSelectOneMenu;
 import at.irian.i18n.jtracc.renderkit.translation.internal.TranslationRenderKit;
-import at.irian.i18n.jtracc.util.BeanUtils;
 import at.irian.i18n.jtracc.util.TranslationUtils;
-import at.irian.i18n.jtracc.util.SettingsUtils;
-import at.irian.i18n.jtracc.util.LocaleUtils;
 import org.apache.myfaces.jtracc.custom.dojolayouts.FloatingPaneBaseRenderer;
 import at.irian.i18n.jtracc.renderkit.translation.internal.TranslationPropertyResolver;
 
 import javax.faces.FactoryFinder;
 import javax.faces.application.Application;
 import javax.faces.application.ApplicationFactory;
-import javax.faces.context.ExternalContext;
 import javax.faces.lifecycle.Lifecycle;
 import javax.faces.lifecycle.LifecycleFactory;
 import javax.faces.render.RenderKitFactory;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.util.Locale;
 
 public class TranslationContextListener implements ServletContextListener
 {
@@ -61,15 +55,12 @@ public class TranslationContextListener implements ServletContextListener
         appliation.addComponent( "org.apache.myfaces.FloatingPaneBase", "org.apache.myfaces.jtracc.custom.dojolayouts.FloatingPaneBase" );
         appliation.addComponent( "org.apache.myfaces.HtmlTranslationPane", "at.irian.i18n.jtracc.custom.translationpane.HtmlTranslationPane" );
 
-        ExternalContext externalContext = new ServletExternalContextImpl( servletContextEvent.getServletContext(), null, null );
-
         // create Settings-Bean for component settings
-        String configFile = externalContext.getInitParameter( Settings.WEB_XML_CONTEXT_PARAMETER_PARAM_NAME_EXT_CONFIG );
-
+        String configFile = servletContextEvent.getServletContext().getInitParameter(Settings.WEB_XML_CONTEXT_PARAMETER_PARAM_NAME_EXT_CONFIG);
         Settings settings = new Settings( configFile );
-        BeanUtils.setApplicationBean( externalContext, Settings.BEAN_NAME, settings );
+        servletContextEvent.getServletContext().setAttribute( Settings.BEAN_NAME, settings );
 
-        setupMessagesBean( externalContext, settings );
+        setupMessagesBean( servletContextEvent, settings );
 
         TranslationRenderKit trk = new TranslationRenderKit( settings.getValue( "render_kit" ) );
         trk.addRenderer( "javax.faces.Output", "org.apache.myfaces.FloatingPaneBaseRenderer", new FloatingPaneBaseRenderer() );
@@ -78,7 +69,7 @@ public class TranslationContextListener implements ServletContextListener
         renderFactory.addRenderKit( TranslationRenderKit.TRANSLATION_RENDERKIT_ID, trk );
     }
 
-    private void setupMessagesBean(ExternalContext externalContext, Settings settings)
+    private void setupMessagesBean(ServletContextEvent servletContextEvent, Settings settings)
     {
         String messagesBeanName = settings.getValue( "messages_bean_name" );
 
@@ -95,7 +86,7 @@ public class TranslationContextListener implements ServletContextListener
             e.printStackTrace();
         }
 
-        BeanUtils.setApplicationBean( externalContext, messagesBeanName, messagesBean );
+        servletContextEvent.getServletContext().setAttribute( messagesBeanName, messagesBean );
     }
 
     public void contextDestroyed(ServletContextEvent servletContextEvent)
